335cb6ebfb10af756a865e08feb1169cc7e29d6f
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2022-11-30 10:02:59 +0330

    test library on remote

--- src/Remote.jsx
+++ src/Remote.jsx
@@ -1,5 +1,20 @@
-const Remote = () => {
-  return <h1>This is Remote</h1>;
+import Typewriter from "typewriter-effect";
+
+const Remote = ({ isRemote = false }) => {
+  return (
+    <div>
+      <h1>This is Remote</h1>
+      {isRemote && (
+        <Typewriter
+          options={{
+            strings: ["This is comming from remote app!"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Remote;
